f8be6ec368a956ae7bffd34fce4ed29a83e460f2
syoui <>
2018-01-14 19:52:37 +0900

    b

--- app/src/main/java/com/example/syoui/imagetab/foundation/fragment/FragmentListenerActivity.java
+++ app/src/main/java/com/example/syoui/imagetab/foundation/fragment/FragmentListenerActivity.java
@@ -1,15 +1,13 @@
 package com.example.syoui.imagetab.foundation.fragment;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.syoui.imagetab.R;
 
@@ -19,65 +17,27 @@ public class FragmentListenerActivity extends AppCompatActivity {
 
 
     private ArrayList<String> mList = null;
-    private FragmentListenerFragment mFragment = null;
     private Context mContext = null;
     private ListView mListView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_listener);
-        mList = new ArrayList<String>();
-        mList.add("nana");
-        mList.add("yuri");
-        mList.add("liaoliao");
-        mList.add("tsutsu");
-        mList.add("techi");
-        mList.add("taiki");
-        mList.add("yuya");
-        mList.add("mayu");
-        mList.add("gemo");
-        mList.add("ruby");
 
-        mListView = (ListView) findViewById(R.id.mainList);
+        final EditText mEditText = (EditText) findViewById(R.id.mEdit);
 
-        ArrayAdapter<String> mListAdapter = new ArrayAdapter<String>(this.getApplicationContext(), android.R.layout.simple_list_item_1,mList);
-        mListView.setAdapter(mListAdapter);
+        mEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
 
-        mContext = this.getApplicationContext();
-
-        mFragment = new FragmentListenerFragment();
-
-        mFragment.setmInterActivationBetweenActivityAndFragment(new FragmentListenerFragment.interActivationBetweenActivityAndFragment() {
-            @Override
-            public void changeListItem(int i, String str) {
-                mList.set(i,str);
-                ArrayAdapter<String> mListAdapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1,mList);
-                mListView.setAdapter(mListAdapter);
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    if ((keyCode == KeyEvent.KEYCODE_ENTER) ||
+                            (keyCode == KeyEvent.KEYCODE_DPAD_CENTER)) {
+                            String res = mEditText.getText().toString();
+                            ((TextView)findViewById(R.id.mText)).setText(res);
+                    }
+                }
+                return true;
             }
         });
-
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if(mFragment == null) return;
-                mFragment.setEditText(i);
-                showFragment(mFragment);
-            }
-        });
-
-
-
     }
-
-
-    private void showFragment(Fragment fragment){
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        fragmentTransaction.add(R.id.fragment_container, fragment);
-        fragmentTransaction.commit();
-    }
-
 }
-
