0928142db9e53f13b8b7591bc243a7585c9d878c
syoui <xu171819411@gmail.com>
2018-11-21 11:05:35 +0900

    styleの使い方

---
+++ app/src/main/java/com/example/syoui/imagetab/foundation/activity/foundation_activity_style.java
@@ -0,0 +1,15 @@
+package com.example.syoui.imagetab.foundation.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.syoui.imagetab.R;
+
+public class foundation_activity_style extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foundation_style);
+    }
+}
